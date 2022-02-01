ca7c11d6495d2ca695f69c023d5ca879a4e3de84
Jack Peplinski <30737750+jackpeplinski@users.noreply.github.com>
2022-02-01 11:44:48 -0500

    Feat: Pass obj to form

--- client/src/Pages/Validate.js
+++ client/src/Pages/Validate.js
@@ -4,14 +4,25 @@ import ApproFields from "../components/ApproFields";
 import OSHWAForm from "../components/OSHWAForm";
 
 const Validity = () => {
-  // valid = true OR [] of missing fields
-  const valid = true;
+  // valid = {} of partial OSHWA data OR [] of missing fields
+  const valid = {
+    responsiblePartyType: "Organization", //r "Organization"
+    responsibleParty: "Affli", //r [Affliations]
+    bindingParty: "Jack", //r [Page authors]
+    country: "Canada", //r [Map result]
+    projectName: "Title", //r [Title]
+    projectWebsite: "URL", // [URL]
+    primaryType: "Some primary type", //r [Uses]
+    hardwareLicense: "Other", //r "Other"
+    softwareLicense: "Other", //r "Other"
+    documentationLicense: "Other", //r "Other"
+  };
   return (
     <Container maxWidth="md">
       {Array.isArray(valid) ? (
         <ApproFields missingFields={valid} />
       ) : (
-        <OSHWAForm />
+        <OSHWAForm partialOSHWAData={valid} />
       )}
     </Container>
   );
