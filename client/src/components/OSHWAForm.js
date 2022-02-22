664829ecd3d9dbe0faf9d3389d0cf4e0ee79ef18
Jack Peplinski <30737750+jackpeplinski@users.noreply.github.com>
2022-02-22 12:34:13 -0500

    Fix: Remove mui warnings

--- client/src/components/OSHWAForm.js
+++ client/src/components/OSHWAForm.js
@@ -70,73 +70,66 @@ const OSHWAForm = (props) => {
         We've already got most of the data we need! Just fill out the few fields
         below.
       </Typography>
-      <div>
-        <form onSubmit={formik.handleSubmit}>
-          <div>
-            {OSHWATruthFields.map((element, index) => (
-              <div style={{ marginBottom: "2vh" }} key={index}>
-                <Typography variant="body1">{element.description}</Typography>
-                <FormControl fullWidth sx={{ margin: "2vh 0" }}>
-                  <InputLabel>
-                    {transformOSHWAField(element.OSHWAField)}
-                  </InputLabel>
-                  <Select
-                    label={transformOSHWAField(element.OSHWAField)}
-                    name={element.OSHWAField}
-                    onChange={formik.handleChange}
-                  >
-                    <MenuItem value={true}>True</MenuItem>
-                    <MenuItem value={false}>False</MenuItem>
-                  </Select>
-                  {formik.values[element.OSHWAField] == false && (
-                    <TextField
-                      placeholder="Required if answered false above."
-                      name={element.explanationField}
-                      multiline
-                      rows={2}
-                      maxRows={4}
-                      onChange={formik.handleChange}
-                    />
-                  )}
-                </FormControl>
-                <Divider />
-              </div>
-            ))}
-            {OSHWAAgreeFields.map((element, index) => (
-              <div style={{ marginBottom: "2vh" }} key={index}>
-                <FormControlLabel
-                  control={<Checkbox />}
-                  label={element.term}
-                  name={element.OSHWAField}
-                  onChange={formik.handleChange}
-                />
-              </div>
-            ))}
-            {OSHWATextFields.map((element, index) => (
-              <div style={{ margin: "2vh 0" }} key={index}>
-                <Typography variant="body1">{element.description}</Typography>
+      <form onSubmit={formik.handleSubmit}>
+        {OSHWATruthFields.map((element, index) => (
+          <div style={{ marginBottom: "2vh" }} key={index}>
+            <Typography variant="body1">{element.description}</Typography>
+            <FormControl fullWidth sx={{ margin: "2vh 0" }}>
+              <InputLabel>{transformOSHWAField(element.OSHWAField)}</InputLabel>
+              <Select
+                defaultValue=""
+                label={transformOSHWAField(element.OSHWAField)}
+                name={element.OSHWAField}
+                onChange={formik.handleChange}
+              >
+                <MenuItem value={true}>True</MenuItem>
+                <MenuItem value={false}>False</MenuItem>
+              </Select>
+              {formik.values[element.OSHWAField] == false && (
                 <TextField
-                  fullWidth
-                  name={element.OSHWAField}
+                  placeholder="Required if answered false above."
+                  name={element.explanationField}
                   multiline
                   rows={2}
-                  maxRows={4}
                   onChange={formik.handleChange}
                 />
-              </div>
-            ))}
-            <FormControlLabel
-              name="agreementTerms"
-              onChange={formik.handleChange}
-              control={<Checkbox />}
-              label="I agree to the terms of the OSHWA Open Source Hardware Certification Mark License Agreement, including the Requirements for Certification and Usage Guidelines incorporated by reference and including license terms that are not present in or conflict with this web form. I acknowledge that by agreeing to the terms of the OSHWA Open Source Hardware Certification Mark License Agreement that I am binding the entity listed to the License Agreement. I recognize that I will receive my unique identification number that allows me to promote my project as OSHWA Open Source Hardware Certified in compliance with the user guidelines via the email provided to OSHWA after submitting this form."
-            />
-            <Button fullWidth color="primary" variant="contained" type="submit">
-              Submit
-            </Button>
+              )}
+            </FormControl>
+            <Divider />
           </div>
-        </form>
-      </div>
+        ))}
+        {OSHWAAgreeFields.map((element, index) => (
+          <div style={{ marginBottom: "2vh" }} key={index}>
+            <FormControlLabel
+              control={<Checkbox />}
+              label={element.term}
+              name={element.OSHWAField}
+              onChange={formik.handleChange}
+            />
+          </div>
+        ))}
+        {OSHWATextFields.map((element, index) => (
+          <div style={{ margin: "2vh 0" }} key={index}>
+            <Typography variant="body1">{element.description}</Typography>
+            <TextField
+              fullWidth
+              name={element.OSHWAField}
+              multiline
+              rows={2}
+              onChange={formik.handleChange}
+            />
+          </div>
+        ))}
+        <FormControlLabel
+          name="agreementTerms"
+          onChange={formik.handleChange}
+          control={<Checkbox />}
+          label="I agree to the terms of the OSHWA Open Source Hardware Certification Mark License Agreement, including the Requirements for Certification and Usage Guidelines incorporated by reference and including license terms that are not present in or conflict with this web form. I acknowledge that by agreeing to the terms of the OSHWA Open Source Hardware Certification Mark License Agreement that I am binding the entity listed to the License Agreement. I recognize that I will receive my unique identification number that allows me to promote my project as OSHWA Open Source Hardware Certified in compliance with the user guidelines via the email provided to OSHWA after submitting this form."
+        />
+        <Button fullWidth color="primary" variant="contained" type="submit">
+          Submit
+        </Button>
+      </form>
     </React.Fragment>
   );
 };
