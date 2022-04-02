861f648135c1aa662afeb6ed6bb9f6ccbfdb0ccd
Jack Peplinski <30737750+jackpeplinski@users.noreply.github.com>
2022-04-02 15:06:27 -0400

    Feat: Update missing info page

--- client/src/Components/ApproFields.js
+++ client/src/Components/ApproFields.js
@@ -1,14 +1,30 @@
-import { List, ListItemButton, ListItemText, Typography } from "@mui/material";
+import {
+  Button,
+  List,
+  ListItemButton,
+  ListItemText,
+  Typography,
+} from "@mui/material";
 import React from "react";
+import { useParams } from "react-router-dom";
 
 const ApproFields = (props) => {
+  const params = useParams();
+  const pageName = params?.pageName;
+
   return (
     <div>
       <Typography variant="h3" component="h3" align="center">
         Please correct these items on the Appropedia page.
       </Typography>
       <Typography variant="h6" component="h6" align="center">
-        Click to learn more!
+        Visit{" "}
+        <a
+          href="https://www.appropedia.org/Appropedia:OSHWA_Certification_tool_design/Documentation#Feedback"
+        >
+          this page
+        </a>{" "}
+        to learn more.
       </Typography>
       <List>
         {props.missingFields.map((field) => (
@@ -17,6 +33,20 @@ const ApproFields = (props) => {
           </ListItemButton>
         ))}
       </List>
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          margin: "2vh",
+        }}
+      >
+        <Button
+          variant="contained"
+          href={`https://www.appropedia.org/w/index.php?title=${pageName}&action=edit`}
+        >
+          Make Changes
+        </Button>
+      </div>
     </div>
   );
 };
